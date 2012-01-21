c9bd1d782621cc0491d0c28d349989c7b985db27
Ilya Yakupov <iyakupov93@gmail.com>
2012-01-21 20:25:03 +0400

    hw4: sse4 also ready, need to write selector

--- hw4/mulvectors.s
+++ hw4/mulvectors.s
@@ -12,7 +12,7 @@ mulVectors:
 	mov	DWORD PTR [ebp-4], 0	/* still res, 'cause we can't mov QWORD */
 	mov	DWORD PTR [ebp-12], 0	/* i */
 
-	jmp mulVectors_SSE2
+	jmp mulVectors_SSE41
 
 mulVectors_FPU:
 //for i = 0..sz
@@ -70,7 +70,53 @@ mulVectors_SSE2:
 
 
 mulVectors_SSE41:
-	//TODO calc
+	subps	xmm0, xmm0		/* result */
+	push	ebx
+	mov	eax, DWORD PTR [ebp+16]	/* our sz */
+	mov	ebx, eax
+	xor	ebx, 1
+	mov	DWORD PTR [ebp-8], ebx	/* evenized sz */
+	xor	ebx, eax		/* "one more element left" flag */
+	jmp	.L7
+
+//for i = 0..sz	
+.L6:
+	mov	eax, DWORD PTR [ebp-12]
+	sal	eax, 3
+	add	eax, DWORD PTR [ebp+8]	/* 1st arg */
+	movupd	xmm1, [eax]
+	mov	eax, DWORD PTR [ebp-12]
+	sal	eax, 3
+	add	eax, DWORD PTR [ebp+12]	/* 2nd arg */
+	movupd	xmm2, [eax]
+	dppd	xmm1, xmm2, 243
+	addsd	xmm0, xmm1
+	inc	DWORD PTR [ebp-12]
+	inc	DWORD PTR [ebp-12]
+
+.L7:
+	mov	eax, DWORD PTR [ebp-12]
+	cmp	eax, DWORD PTR [ebp-8]	/* sz */
+	jb	.L6
+//end of for i = 0..sz
+
+	or 	ebx, ebx
+	jz .L8
+//process last element
+	mov	eax, DWORD PTR [ebp-12]
+	sal	eax, 3
+	add	eax, DWORD PTR [ebp+8]	/* 1st arg */
+	movlpd	xmm1, [eax]
+	mov	eax, DWORD PTR [ebp-12]
+	sal	eax, 3
+	add	eax, DWORD PTR [ebp+12]	/* 2nd arg */
+	movlpd	xmm2, [eax]
+	mulsd	xmm2, xmm1
+	addsd	xmm0, xmm2
+.L8:
+	pop	ebx
+	movsd	QWORD PTR [ebp-8], xmm0
 	fld	QWORD PTR [ebp-8]	/* return */
 	leave
 	ret
+
